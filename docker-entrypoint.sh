e514aaef4d7248277f0d26cfda57e9ede6b8351c
Jakub Mucha <jakub.mucha@icloud.com>
2021-05-27 22:30:46 +0200

    chmod _w
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 sh -c "sudo chown -R root /github"
 sh -c "sudo chmod -R 777 /github"
+sh -c "sudo chown -R root /__w"
+sh -c "sudo chmod -R 777 /__w"
 
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
   set -- node "$@"
