7d6ad535d46e1884d134bbde8128a93642bd7950
Jakub Mucha <jakub.mucha@icloud.com>
2021-05-27 22:23:26 +0200

    chmod github folder
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+sh -c "sudo chown -R root /github"
+sh -c "sudo chmod -R 777 /github"
+
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
   set -- node "$@"
 fi
