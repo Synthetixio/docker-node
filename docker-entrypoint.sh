09e3d8eb8bb52e529fdc603d9f7112d99a3d08db
Jakub Mucha <jakub.mucha@icloud.com>
2021-06-01 22:50:22 +0200

    cleanup
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-sh -c "sudo chown -R root /github"
-sh -c "sudo chmod -R 777 /github"
-sh -c "sudo chown -R root /__w"
-sh -c "sudo chmod -R 777 /__w"
-
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
   set -- node "$@"
 fi
